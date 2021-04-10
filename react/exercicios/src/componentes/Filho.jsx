f01e65d3cb50ac89044c310c346be16261950094
Eduardo Froelich <eduardofroelich.dev@gmail.com>
2021-04-10 15:53:45 -0300

    Adds react/

---
+++ react/exercicios/src/componentes/Filho.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+// eslint-disable-next-line
+export default props => <li>{props.nome} {props.sobrenome}</li>
---
+++ react/exercicios/src/index.js
@@ -0,0 +1,19 @@
+// npm i -g create-react-app
+// create-react-app exercicios
+// npm start
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
+
+ReactDom.render(
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
+        </Pai>
+    </div>
+    , document.getElementById('root'))
